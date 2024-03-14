d8660c05e86760fcf1ee47e81f60c7fc7678c783
kruthi-msd <106132887+kruthi-msd@users.noreply.github.com>
2024-03-14 11:38:43 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -2,21 +2,21 @@ class sumOfNum
 {
     public static void main(String args[])
     {
-        int[] values = {10,51,53,39,47,44,21};
-        int n=values.length;
+        int[] val= {10,51,53,39,47,44,21};
+        int n=val.length;
         for(int i=0;i<n;i++)
         {
             for(int j=i;j<n;j++)
             {
-                if(values[i]>values[j])
+                if(val[i]>val[j])
                 {
-                    int temp=values[j];
-                    values[j]=values[i];
-                    values[i]=temp;
+                    int temp=val[j];
+                    val[j]=val[i];
+                    val[i]=temp;
                 }
             }
         }
-        System.out.print("Sum of 2nd and 3rd largest number : ");
-        System.out.println(values[n-2]+values[n-3]);
+        System.out.print("Sum of 2nd,3rd largest numbers : ");
+        System.out.println(val[n-2]+val[n-3]);
     }
 }
